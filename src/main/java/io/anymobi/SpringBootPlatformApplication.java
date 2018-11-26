a87f1dc5534ec98b1c88fb084ece565432bfa348
onjsdnjs <onjsdnjs@gmail.com>
2018-11-26 18:28:34 +0900

    first commit

---
+++ src/main/java/io/anymobi/SpringBootPlatformApplication.java
@@ -0,0 +1,12 @@
+package io.anymobi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPlatformApplication.class, args);
+    }
+}
